288e2a76951c3b7d7485aa5b605eb06175d55fbd
Phyrone <phyrone@phyrone.de>
2026-08-03 13:55:45 +0200

    🏗️ Added vitest and playwright

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,9 +1,8 @@
 import { allPosts } from '$content';
 import Fuse from 'fuse.js';
-import { } from 'fuse.js';
+import {} from 'fuse.js';
 
 export const postsSearch = new Fuse(allPosts, {
-  keys: ['title', 'description'],
-  includeScore: true,
-  
+	keys: ['title', 'description'],
+	includeScore: true
 });
